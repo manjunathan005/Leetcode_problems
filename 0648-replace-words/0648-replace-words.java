9c3cab100def62306520b33d3058b8dcdcab4f2d
manjunathan005 <manjunathan0322@gmail.com>
2026-07-23 14:34:22 +0530

    Time: 701 ms (14.56%), Space: 59.2 MB (76.88%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set <String>set = new HashSet<>(dictionary);
+
+        String[]arr = sentence.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String a: arr){
+            String replace = a;
+
+            for(int j=0;j<= replace.length();j++){
+                String newword = replace.substring(0,j);
+                if(set.contains(newword)){
+                    replace = newword;
+                    break;
+                }
+            }
+            sb.append(replace).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
