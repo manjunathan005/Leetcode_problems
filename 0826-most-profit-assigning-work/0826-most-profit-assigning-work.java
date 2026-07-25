26a7b0db447da8b9d0a28bd8556889e9a1608eb2
manjunathan005 <manjunathan0322@gmail.com>
2026-07-25 12:05:26 +0530

    Time: 21 ms (88.99%), Space: 48.2 MB (77.58%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+
+        int n = difficulty.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = difficulty[i];
+            arr[i][1] = profit[i];
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(worker);
+        int total = 0;
+        int pro = 0;
+        int i = 0;
+
+        for (int j : worker) {
+            while (i < n && arr[i][0] <= j) {
+                pro = Math.max(pro, arr[i][1]);
+                i++;
+            }
+
+            total += pro;
+        }
+
+        return total;
+    }
+}
