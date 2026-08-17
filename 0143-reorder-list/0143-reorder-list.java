797ec77730488196e67554cc937067a8b3117dad
manjunathan005 <manjunathan0322@gmail.com>
2026-08-17 14:54:41 +0530

    Time: 2 ms (87.05%), Space: 49.1 MB (87.44%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode  slow  = head;
+        ListNode fast = head;
+        while(fast !=null && fast.next !=null){
+            slow  = slow.next;
+            fast = fast.next.next;
+
+        }
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+        while(second !=null){
+            ListNode next= second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+            
+        }
+        second = prev;
+        ListNode first = head;
+        while(second !=null){
+            ListNode temp = first.next;
+             ListNode temp1 = second.next;
+             first.next = second;
+             second.next = temp;
+             first = temp;
+             second = temp1;
+
+        }
+       
+    }
+}
